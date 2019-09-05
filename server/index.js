92ce489c8337666d50a162c3fc3f47e2b53c7dfb
Jesse Forrester <jesse.forrester@yahoo.com>
2019-09-04 17:19:21 -0700

    CSS adjustments

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/listings/:id', (req, res) => {
+  const { id } = req.params;
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+} )
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
